1f3f58775b58028d6f8351e5204fa5f026e17b17
darrylrayhananta <19623049@std.stei.itb.ac.id>
2025-05-10 17:39:47 +0700

    feat: update season.java

--- src/Season.java
+++ src/Season.java
@@ -1,10 +1,10 @@
 public class Season {
-    private String[] seasonCycle = {"Spring", "Summer", "Fall", "Winter"};
+    private String[] season = {"Spring", "Summer", "Fall", "Winter"};
     private int currentSeason = 0;
     private int dayCounter = 0;
 
     public String getCurrentSeason() {
-        return seasonCycle[currentSeason];
+        return season[currentSeason];
     }
 
     public int getDayInSeason() {
@@ -15,14 +15,14 @@ public class Season {
         dayCounter++;
         if (dayCounter >= 10) {
             dayCounter = 0;
-            currentSeason = (currentSeason + 1) % seasonCycle.length;
+            currentSeason = (currentSeason + 1) % season.length;
         }
     }
 
     // Cheat tool
     public void setSeason(String seasonName) {
-        for (int i = 0; i < seasonCycle.length; i++) {
-            if (seasonCycle[i].equalsIgnoreCase(seasonName)) {
+        for (int i = 0; i < season.length; i++) {
+            if (season[i].equalsIgnoreCase(seasonName)) {
                 currentSeason = i;
                 dayCounter = 0;
                 break;
